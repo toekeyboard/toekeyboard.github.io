04651a325991e3c4feda6be423352cb0a1513e86
! <>
2017-08-31 12:43:57 -0400

    .

--- board.net/s.sh
+++ board.net/s.sh
@@ -1,3 +1,4 @@
+rm txt
 wget http://board.net/p/toekeyboard/export/txt
 #wget http://board.net/p/1674927471/export/txt
 
@@ -8,14 +9,16 @@ if [ -z "$diff_output" ]; then
 fi
 
 
-cp txt txt_old
-
-#fldiff txt_old txt
-#fldiff txt txt_old
 
 echo "<pre>" > txt.html
 cat txt >> txt.html
 echo "</pre>" >> txt.html
+
+
+cp txt txt_old
+
+#fldiff txt_old txt
+#fldiff txt txt_old
 
 #echo "Press Ctrl-C to cancel committing to git"
 #sleep 2
